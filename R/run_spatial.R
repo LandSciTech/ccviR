009755c8bdc94d3f07806ec0a6432703079329dc
see24 <sarahendicott4@gmail.com>
2022-05-24 10:50:26 -0400

    fix #77 add a warning message if polygon shapefiles have many rows

--- R/run_spatial.R
+++ R/run_spatial.R
@@ -238,6 +238,12 @@ check_polys <- function(poly, rast_crs, var_name){
   poly <- valid_or_error(poly, var_name)
 
   if(nrow(poly) > 1){
+    if(nrow(poly) > 100){
+      warning("The", var_name, "povided contains more than 100 polygons. ",
+              "These will be unioned to create one polygon. ",
+              "If this is not expected please use a different shapefile.",
+              call. = FALSE)
+    }
     poly <- sf::st_union(poly) %>% sf::st_as_sf()
   }
 
