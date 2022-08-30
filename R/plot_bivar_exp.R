45ce5ad894a75675b037420d4e66440751dd1478
see24 <sarahendicott4@gmail.com>
2022-08-30 15:14:23 -0400

    finish rename and document bivar_exp

--- R/plot_bivar_exp.R
+++ R/plot_bivar_exp.R
@@ -1,3 +1,33 @@
+#' Plot bivariate exposure map
+#'
+#' Create a map of exposure to climate change based on both change in
+#' temperature and change in climate moisture deficit.
+#'
+#' @param mat RasterLayer of classified mean annual temperature exposure
+#' @param cmd RasterLayer of classified climate moisture deficit exposure
+#' @param leg_rel_size numeric, shrinkage of the legend size relative to the
+#'   plot. Default is 2.5 larger numbers will make the legend smaller
+#' @param palette named vector of colours in each corner of the bivariate scale.
+#'   Required names are bottomleft, bottomright, upperleft, and upperright.
+#'
+#' @return a ggplot object
+#' @export
+#'
+#' @examples
+#' # load the demo data
+#' file_dir <- system.file("extdata", package = "ccviR")
+#'
+#' # scenario names
+#' scn_nms <- c("RCP 4.5", "RCP 8.5")
+#'
+#' clim_vars <- get_clim_vars(file.path(file_dir, "clim_files/processed"),
+#'                            scn_nms)
+#'
+#' mat <- clim_vars$mat$RCP_4.5
+#' cmd <- clim_vars$cmd$RCP_4.5
+#'
+#' plot_bivar_exp(mat, cmd)
+#'
 plot_bivar_exp <- function(mat, cmd, leg_rel_size = 2.5,
                            palette = c(bottomleft = "green", bottomright = "blue",
                                        upperleft = "orange", upperright = "magenta")){
