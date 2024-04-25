8b40b792b025c40fda011162d5d7b52172100368
Adriana Caswell <adriana.caswell@ec.gc.ca>
2024-04-25 11:30:14 -0400

    changed all tables to gt and updated HTN table #149

--- R/get_exposure_table.R
+++ R/get_exposure_table.R
@@ -2,14 +2,14 @@ get_exposure_table <- function(spattbl, varname, clim_readme, brks){
 
   if(varname == "MAT"){
     exp_val <- "temp_exp_cave"
-    class_col <- html("Temperature class")
-    breaks_col <- html("Temperature change (&deg;C)")
+    class_col <- gt::html("Temperature class")
+    breaks_col <- gt::html("Temperature change (&deg;C)")
   }
 
   if(varname == "CMD"){
     exp_val <- "moist_exp_cave"
-    class_col <- html("Moisture class")
-    breaks_col <- html("Moisture change (mm)")
+    class_col <- gt::html("Moisture class")
+    breaks_col <- gt::html("Moisture change (mm)")
   }
 
   scenario_cols <- spattbl$scenario_name
@@ -44,24 +44,24 @@ get_exposure_table <- function(spattbl, varname, clim_readme, brks){
 
     # create table
     exp_res_tbl <- gt::gt(exp_res_df) %>%
-      cols_label(class = class_col, breaks = breaks_col) %>%
-      tab_spanner(label = "Proportion of range", columns = scenario_cols) %>%
-      tab_options(table.width = 600,
-                  table.font.size = 12,
+      gt::cols_label(class = class_col, breaks = breaks_col) %>%
+      gt::tab_spanner(label = "Proportion of range", columns = scenario_cols) %>%
+      gt::tab_options(table.width = 600,
+                  table.font.size = 14,
                   column_labels.padding.horizontal = 10,
                   column_labels.padding = 2,
                   data_row.padding = 2) %>%
-      cols_align(align = "center", columns = everything()) %>%
-      sub_missing(missing_text = "") %>%
-      tab_style(style = cell_text(weight = "bold", v_align = "middle"),
-                location = cells_column_labels(columns = 1:2)) %>%
-      tab_style(style = list(cell_text(weight = "bold")),
-                location = cells_column_spanners()) %>%
-      tab_style_body(style = cell_text(weight = "bold"),
+      gt::cols_align(align = "center", columns = everything()) %>%
+      gt::sub_missing(missing_text = "") %>%
+      gt::tab_style(style = gt::cell_text(weight = "bold", v_align = "middle"),
+                location = gt::cells_column_labels(columns = 1:2)) %>%
+      gt::tab_style(style = list(gt::cell_text(weight = "bold")),
+                location = gt::cells_column_spanners()) %>%
+      gt::tab_style_body(style = gt::cell_text(weight = "bold"),
                      values = "Exposure Multiplier") %>%
-      tab_style_body(style = list(cell_fill(color = "#F8F8F8"),
-                                  cell_borders(side = c("top", "bottom"),
-                                               weight = px(2), color = "lightgray")),
+      gt::tab_style_body(style = list(gt::cell_fill(color = "#F8F8F8"),
+                                      gt::cell_borders(side = c("top", "bottom"),
+                                               weight = gt::px(2), color = "lightgray")),
                      values = "Exposure Multiplier", targets = "row")
   } else {
 
@@ -101,23 +101,23 @@ get_exposure_table <- function(spattbl, varname, clim_readme, brks){
 
     # create table
     exp_res_tbl <- gt::gt(exp_res_df) %>%
-      cols_label(class = class_col, breaks = breaks_col, value = "Proportion of range") %>%
-      tab_options(table.width = 600,
-                  table.font.size = 12,
+      gt::cols_label(class = class_col, breaks = breaks_col, value = "Proportion of range") %>%
+      gt::tab_options(table.width = 600,
+                  table.font.size = 14,
                   column_labels.padding.horizontal = 10,
                   column_labels.padding = 2,
                   data_row.padding = 2) %>%
-      cols_align(align = "center", columns = everything()) %>%
-      sub_missing(missing_text = "") %>%
-      tab_style(style = cell_text(weight = "bold", v_align = "middle"),
-                location = cells_column_labels()) %>%
-      tab_style(style = cell_text(weight = "bold"),
-                location = cells_row_groups()) %>%
-      tab_style_body(style = cell_text(weight = "bold"),
+      gt::cols_align(align = "center", columns = everything()) %>%
+      gt::sub_missing(missing_text = "") %>%
+      gt::tab_style(style = gt::cell_text(weight = "bold", v_align = "middle"),
+                location = gt::cells_column_labels()) %>%
+      gt::tab_style(style = gt::cell_text(weight = "bold"),
+                location = gt::cells_row_groups()) %>%
+      gt::tab_style_body(style = gt::cell_text(weight = "bold"),
                 values = "Exposure Multiplier") %>%
-      tab_style_body(style = list(cell_fill(color = "#F8F8F8"),
-                             cell_borders(side = c("top", "bottom"),
-                                          weight = px(2), color = "lightgray")),
+      gt::tab_style_body(style = list(gt::cell_fill(color = "#F8F8F8"),
+                                      gt::cell_borders(side = c("top", "bottom"),
+                                          weight = gt::px(2), color = "lightgray")),
                 values = "Exposure Multiplier", targets = "row")
   }
 
