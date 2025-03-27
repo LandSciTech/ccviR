f32eb60effd59a4836bdb99053c296407bd5de96
Steffi LaZerte <steffi@steffi.ca>
2025-03-27 14:16:31 -0500

    Update protected areas - Simpler US data - use st_simplify rather than mapshaper - Add notes - Add more checks

--- data-raw/data_protected.R
+++ data-raw/data_protected.R
@@ -1,3 +1,20 @@
+# Notes ----------------------------
+
+## Simplifications ------------------------------------------
+# Use st_simplify vs. rmapshaper::ms_simplify()
+# - We don't require the preserved matching among polygon border that
+#   mapshaper uses and dTolerance of st_simplify is simpler to understand.
+# - use preserveTopology = TRUE in `st_simplify()` to keep small islands
+# - Use dTolerance of 50m (amount of simplification - related to distance between vertices to be removed and edges)
+
+# References
+# - https://r.geocompx.org/geometry-operations.html?q=simplify#simplification
+# - https://pro.arcgis.com/en/pro-app/latest/tool-reference/cartography/simplify-polygon.htm
+
+# Other notes
+# - Transform CRS after simplification because starting CRS are appropriate
+# - Use ESRI:102008 which is a North American Equal Albers projection (https://epsg.io/102008)
+
 
 # Setup -----------------------------------------------------
 
@@ -6,12 +23,6 @@ library(fs)
 library(glue)
 library(sf)
 library(dplyr)
-library(rmapshaper) # For fast, topologically-aware simplifications
-
-# Check that you have the system mapshaper installed
-# (if not: https://andyteucher.ca/rmapshaper/articles/rmapshaper.html#using-the-system-mapshaper)
-# Need to use system mapshaper because R can't handle these large files
-check_sys_mapshaper()
 
 # Setup file paths
 dir_pa <- path("misc", "protected_areas")
@@ -20,7 +31,7 @@ dir_USA <- path(dir_pa, "USA")
 dir_create(c(dir_pa, dir_CAN, dir_USA))
 
 # Coordinate reference systems - Preserve area
-crs_out <- ccviR::crs_albers_na()
+crs_out <- "ESRI:102008"
 
 # Download data ----------------------------------------------------------------
 
@@ -35,12 +46,14 @@ if(!file_exists(f_CAN)) {
 }
 
 ## USA Government ----------------------------------
+# Use Vector Analysis and Summary Statistics data set (not 'Full Inventory')
 # https://www.usgs.gov/programs/gap-analysis-project/science/pad-us-data-download
-# https://www.sciencebase.gov/catalog/item/652ef930d34edd15305a9b03
-f_USA <- path(dir_USA, "PADUS4_0Geodatabase.zip")
+# https://www.sciencebase.gov/catalog/item/652d4ebbd34e44db0e2ee458
+
+f_USA <- path(dir_USA, "PADUS4_0VectorAnalysis_GAP_PADUS_Only_ClipCENSUS.zip")
 if(!file_exists(f_USA)) {
   # May need to get link by hand every time from sciencebase URL (above)
-  request("https://prod-is-usgs-sb-prod-content.s3.amazonaws.com/652ef930d34edd15305a9b03/PADUS4_0Geodatabase.zip?AWSAccessKeyId=AKIAI7K4IX6D4QLARINA&Expires=1737735173&Signature=W2jrMRgpldQAtOcBvCWKTZxcybA%3D") %>%
+  request("https://prod-is-usgs-sb-prod-content.s3.us-west-2.amazonaws.com/652d4ebbd34e44db0e2ee458/PADUS4_0VectorAnalysis_GAP_PADUS_Only_ClipCENSUS.zip?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Date=20250327T161204Z&X-Amz-SignedHeaders=host&X-Amz-Expires=86399&X-Amz-Credential=AKIAI7K4IX6D4QLARINA%2F20250327%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Signature=bc4591d127825bdab761b4f0a85b3fa271b9d55a21e7e09434eaccac4c2f5f9a") %>%
     req_progress() %>%
     req_perform(path = f_USA)
   unzip(f_USA, exdir = dir_USA)
@@ -77,7 +90,8 @@ ca <- st_read(f_CAN, layer = "ProtectedConservedArea_2023") %>%
   select(
     "ZONE_ID",     # ID
     "IUCN_CAT",    # 1-7 are IUCN Recognized Protected areas, 8 & 9 are unreported or other
-    "BIOME",       # Type of protected area (M - Fully or partially marine; T - Terrestrial or Freshwater"
+    "BIOME",       # Type of protected area (M - Fully or partially marine;
+                   #   T - Terrestrial or Freshwater"
     #"STATUS",      # 0 - Delisted
     #"PA_OECM_DF",  # 1 Protected Area; 2 OECM; 3 Interim PA; 4 Interim OECM; 5 NA
 
@@ -92,12 +106,10 @@ ca <- st_read(f_CAN, layer = "ProtectedConservedArea_2023") %>%
 sf::st_geometry_type(ca) %>% unique()
 
 ### Simplify and union -----------------------------
-# - Use snap_interval of 50m to snap polygons to each other if within 50m
-# - Transform CRS after simplification because starting CRS is appropriate
 
 ca1 <- ca %>%
-  ms_simplify(keep = 0.05, sys = TRUE, snap_interval = 50) %>%
-  st_buffer(0) %>% # Magic Fairy dust: https://github.com/r-spatial/sf/issues/518
+  st_simplify(preserveTopology = TRUE, dTolerance = 50) %>%
+  st_make_valid() %>%  # Could use st_buffer(0) to increase speed (Magic Fairy dust: https://github.com/r-spatial/sf/issues/518)
   st_union() %>%
   st_transform(crs = crs_out)
 
@@ -105,13 +117,10 @@ st_write(ca1, path(dir_pa, "pa_canada.gpkg"), overwrite = TRUE, append = FALSE)
 
 
 
-### Exploration of simplification ------------------------------
+### Explore - Final set of polygons ------------------------------
 if(FALSE) {
-
   library(ggplot2)
-  library(rnaturalearth)
   library(patchwork)
-  library(ggspatial)
 
   # Plot full set of polygons
   ggplot() +
@@ -119,20 +128,106 @@ if(FALSE) {
 
   # How do areas compare before and after simplification?
   # - Union original data to make it comparable
-  # - 98% of area preserved
-  sum(st_area(ca1)) / sum(st_area(st_union(ca))) * 100
+  # - 99.97% of area preserved
+  sum(st_area(ca1)) / sum(st_area(st_union(ca))) * 100 # SLOW
+}
+
+### Explore - Simplification ------------------------------
+if(FALSE) {
+  library(ggplot2)
+  library(patchwork)
 
   sm <- st_bbox(c(xmin = -67.65, xmax = -67.6,
                   ymin = 47.95, ymax = 48.05),
                 crs = 4326)
 
   ca_test <- st_crop(ca, st_transform(sm, st_crs(ca)))
-  ca_test_simp <- ca_test %>%
-    ms_simplify(keep = 0.05, sys = TRUE, snap_interval = 50) %>%
+
+  ca_test_simp0 <- ca_test %>%
+    st_simplify(preserveTopology = TRUE, dTolerance = 0) %>%
     st_union()
 
+  ca_test_simp1 <- ca_test %>%
+      st_simplify(preserveTopology = TRUE, dTolerance = 50) %>%
+      st_union()
+
+  ca_test_simp2 <- ca_test %>%
+    st_simplify(preserveTopology = TRUE, dTolerance = 150) %>%
+    st_union()
+
+
+  # 99% of original area in simplified
+  st_area(ca_test_simp0) / sum(st_area(ca_test)) * 100 # 99.5 %
+  st_area(ca_test_simp1) / sum(st_area(ca_test)) * 100 # 99.3 %
+  st_area(ca_test_simp2) / sum(st_area(ca_test)) * 100 # 98.7 %
+
+  # Object sizes
+  lobstr::obj_size(ca_test)       # 63.44 kB
+  lobstr::obj_size(ca_test_simp0) # 58.06 kB
+  lobstr::obj_size(ca_test_simp1) # 12.48 kB
+  lobstr::obj_size(ca_test_simp2) #  9.16 kB
+
+
+  g0 <- ggplot() +
+    geom_sf(data = ca_test, fill = "yellow") +
+    labs(title = "No simplification") +
+    ggspatial::annotation_scale()
+
+  g1 <- ggplot() +
+    geom_sf(data = ca_test_simp1, fill = "red") +
+    labs(title = "st_simplify, tolerance 0m") +
+    ggspatial::annotation_scale()
+
+  g2 <- ggplot() +
+    geom_sf(data = ca_test_simp1, fill = "red") +
+    labs(title = "st_simplify, tolerance 50m") +
+    ggspatial::annotation_scale()
+
+  g3 <- ggplot() +
+    geom_sf(data = ca_test_simp1, fill = "red") +
+    labs(title = "st_simplify, tolerance 150m") +
+    ggspatial::annotation_scale()
+
+  g0 + g1 + g2 + g3
+}
+
+### Compare simplifications ------------------------------
+# mapshaper (via rmapshaper) vs st_simplify
+# Some more details: https://datascience.blog.wzb.eu/2021/03/15/simplifying-geospatial-features-in-r-with-sf-and-rmapshaper/
+if(FALSE) {
+  library(rmapshaper) # For fast, topologically-aware simplifications
+  library(ggplot2)
+  library(patchwork)
+  library(ggspatial)
+
+  # Check that you have the system mapshaper installed
+  # (if not: https://andyteucher.ca/rmapshaper/articles/rmapshaper.html#using-the-system-mapshaper)
+  # Need to use system mapshaper because R can't handle these large files
+  check_sys_mapshaper()
+
+  sm <- st_bbox(c(xmin = -67.65, xmax = -67.6,
+                  ymin = 47.95, ymax = 48.05),
+                crs = 4326)
+
+  ca_test <- st_crop(ca, st_transform(sm, st_crs(ca)))
+
+  # Sf faster than
+  bench::mark(sf = {
+    ca_test_simp1 <- ca_test %>%
+      st_simplify(preserveTopology = TRUE, dTolerance = 50) %>%
+      st_union()
+  }, rmsp = {
+    ca_test_simp2 <- ca_test %>%
+      ms_simplify(keep = 0.4, sys = TRUE, snap_interval = 50) %>%
+      st_union()
+  }, check = FALSE)
+
   # 92% of original area in simplified
-  st_area(ca_test_simp) / sum(st_area(ca_test)) * 100
+  st_area(ca_test_simp1) / sum(st_area(ca_test)) * 100
+  st_area(ca_test_simp2) / sum(st_area(ca_test)) * 100
+
+  lobstr::obj_size(ca_test_simp1)
+  lobstr::obj_size(ca_test_simp2)
 
   g0 <- ggplot() +
     geom_sf(data = ca_test, fill = "yellow") +
@@ -140,12 +235,18 @@ if(FALSE) {
     annotation_scale()
 
   g1 <- ggplot() +
-    geom_sf(data = ca_test_simp, fill = "red") +
-    labs(title = "Simplified") +
+    geom_sf(data = ca_test_simp1, fill = "red") +
+    labs(title = "sf Simplified") +
     annotation_scale()
 
-  g0 + g1
+  g2 <- ggplot() +
+    geom_sf(data = ca_test_simp2, fill = "red") +
+    labs(title = "mapshaper Simplified") +
+    annotation_scale()
+
+  g0 + g1 + g2
 }
+
 
 ## USA ------------------------------
 # Use st_wrap_dateline() deal with polygons near the date line
@@ -154,55 +255,53 @@ if(FALSE) {
 
 ### Setup ----
 
-f_USA <- path(dir_USA, "PADUS4_0_Geodatabase.gdb")
+f_USA <- path(dir_USA, "PADUS4_0VectorAnalysis_GAP_PADUS_Only_ClipCENSUS.gdb/")
 #st_layers(f_USA)
+us_glimpse <- st_read(
+  f_USA,
+  query = paste(
+    "SELECT *",
+    "FROM PADUS4_0VectorAnalysis_GAP_Simp_SingP_ClipCENSUS",
+    "LIMIT 10"))
 
 # This can take a couple of minutes
 us <- st_read(
   f_USA,
   query = paste(
     "SELECT *",
-    "FROM PADUS4_0Combined_Proclamation_Marine_Fee_Designation_Easement",
-    #"FROM PADUS4_0Marine", # For a quick look
+    "FROM PADUS4_0VectorAnalysis_GAP_Simp_SingP_ClipCENSUS",
     # Using only IUCN recognized
-    "WHERE IUCN_Cat IN ('Ia','Ib', 'II', 'III', 'IV', 'V', 'VI')",
-    # Omitting Marine areas
-    "AND (Category != 'Marine')")) %>%
+    "WHERE IUCN_Cat IN ('Ia','Ib', 'II', 'III', 'IV', 'V', 'VI')")) %>%
   st_cast("MULTIPOLYGON") %>% # Deal with warnings about collections
   mutate(IUCN_Cat = as.numeric(factor(
     IUCN_Cat, levels = c("Ia", "Ib", "II", "III", "IV", "V", "VI")))) %>%
   # Also omit "fee" Features which are identified as Marine by name
   filter(!stringr::str_detect(tolower(Unit_Nm), "marine"))
 
-# Note:
-# - What about "Alaska Maritime National Wildlife Refuge" -> Marine?
-# filter(us, State_Nm %in% c("AK", "HI")) %>%
-#   sf::st_drop_geometry() %>%
-#   select(State_Nm, Unit_Nm, Loc_Nm) %>%
-#   filter(stringr::str_detect(tolower(Unit_Nm), "mari"))
+# Note: What about Maritime and Marine parks?
+# - "Alaska Maritime National Wildlife Refuge" -> Marine?
+# filter(us, stringr::str_detect(tolower(Unit_Nm), "maritime|marine")) |>
+#   st_drop_geometry() |>
+#   select(Unit_Nm) |>
+#   distinct()
 
 # Verify that they are all 'standard' (i.e. MULTIPOLYGONs)
 sf::st_geometry_type(us) %>% unique()
 
 ### Simplify and union -----------------------------
-# - Use snap_interval of 50m to snap polygons to each other if within 50m
-# - Transform CRS after simplification because starting CRS is appropriate
+# Be patient!
 us1 <- us %>%
-  ms_simplify(keep = 0.05, sys = TRUE, snap_interval = 50) %>%
-  st_buffer(0) %>% # Magic Fairy dust: https://github.com/r-spatial/sf/issues/518
+  st_simplify(preserveTopology = TRUE, dTolerance = 50) %>%
+  st_make_valid() %>% # could use st_buffer(0) to increase speed
   st_union() %>%
   st_transform(crs = crs_out)
 
 st_write(us1, path(dir_pa, "pa_usa.gpkg"), overwrite = TRUE, append = FALSE)
 
-
-### Exploration of simplification ------------------------------
+### Explore - Final set of polygons ------------------------------
 if(FALSE) {
-
   library(ggplot2)
-  library(rnaturalearth)
   library(patchwork)
-  library(ggspatial)
 
   # Plot full set of polygons
   ggplot() +
@@ -211,39 +310,80 @@ if(FALSE) {
   # How do areas compare before and after simplification?
   # - Union original data to make it comparable
   # - This takes a long time, so use a smaller region
-  # - 97% of area preserved
   med <- st_bbox(c(xmin = -100.75, xmax = -80.5,
                    ymin = 30, ymax = 40),
                  crs = 4326)
   us0 <- us %>%
     st_crop(st_transform(med, st_crs(us))) %>%
-    st_buffer(0) %>%
+    st_make_valid() %>%  #st_buffer(0) %>%
     st_union()
-  sum(st_area(st_crop(us1, st_transform(med, st_crs(us1))))) / sum(st_area(us0)) * 100
 
+  us1.1 <- st_buffer(us1, 0) %>%
+    st_crop(st_transform(med, st_crs(us1))) %>%
+    st_make_valid() %>% #st_buffer(0) %>%
+    st_union()
 
-  # How do the shapes compare before and after?
+  sum(st_area(us1.1)) / sum(st_area(us0)) * 100
+}
+
+### Explore - Simplification ------------------------------
+if(FALSE) {
+  library(ggplot2)
+  library(patchwork)
+
   # Look at a small area
   sm <- st_bbox(c(xmin = -90.75, xmax = -90.5,
                   ymin = 35, ymax = 35.2),
                 crs = 4326)
 
   us_test <- st_crop(us, st_transform(sm, st_crs(us)))
-  us_test_simp <- us_test %>%
-    ms_simplify(keep = 0.05, sys = TRUE, snap_interval = 50) %>%
+
+  us_test_simp0 <- us_test %>%
+    st_simplify(preserveTopology = TRUE, dTolerance = 0) %>%
     st_union()
+
+  us_test_simp1 <- us_test %>%
+    st_simplify(preserveTopology = TRUE, dTolerance = 50) %>%
+    st_union()
+
+  us_test_simp2 <- us_test %>%
+    st_simplify(preserveTopology = TRUE, dTolerance = 150) %>%
+    st_union()
+
+
+  # 99% of original area in simplified
+  st_area(us_test_simp0) / sum(st_area(us_test)) * 100 # 100 %
+  st_area(us_test_simp1) / sum(st_area(us_test)) * 100 # 99.6 %
+  st_area(us_test_simp2) / sum(st_area(us_test)) * 100 # 99.3 %
+
+  # Object sizes
+  lobstr::obj_size(us_test)       # 40.61 kB
+  lobstr::obj_size(us_test_simp0) # 27.44 kB
+  lobstr::obj_size(us_test_simp1) # 19.70 kB
+  lobstr::obj_size(us_test_simp2) # 17.55 kB
+
 
   g0 <- ggplot() +
     geom_sf(data = us_test, fill = "yellow") +
     labs(title = "No simplification") +
-    annotation_scale()
+    ggspatial::annotation_scale()
 
   g1 <- ggplot() +
-    geom_sf(data = us_test_simp, fill = "red") +
-    labs(title = "Simplified") +
-    annotation_scale()
+    geom_sf(data = us_test_simp0, fill = "red") +
+    labs(title = "st_simplify, tolerance 0m") +
+    ggspatial::annotation_scale()
 
-  g0 + g1
+  g2 <- ggplot() +
+    geom_sf(data = us_test_simp1, fill = "red") +
+    labs(title = "st_simplify, tolerance 50m") +
+    ggspatial::annotation_scale()
+
+  g3 <- ggplot() +
+    geom_sf(data = us_test_simp2, fill = "red") +
+    labs(title = "st_simplify, tolerance 150m") +
+    ggspatial::annotation_scale()
+
+  g0 + g1 + g2 + g3
 }
 
 
