044119287d9cad8aafb3bfc0a116f1bfee5a97d1
Steffi LaZerte <steffi@steffi.ca>
2025-04-07 13:50:44 -0500

    Create mini CCEI data for testing (#25)

---
+++ data-raw/data_ccei_mini.R
@@ -0,0 +1,68 @@
+# Create a mini test data set for CCEI
+
+library(terra)
+library(dplyr)
+library(fs)
+
+# Local paths
+dir_ccei <- path("misc", "ccei")
+dir_hist <- path(dir_ccei, "historical")
+dir_future <- path(dir_ccei, "future")
+
+# Subsample out paths
+out_dir <- path(path_package("extdata", package = "ccviR"), "ccei_test")
+dir_create(out_dir)
+dir_out_future <- dir_create(path(out_dir, "future"))
+dir_out_hist <- dir_create(path(out_dir, "historical"))
+
+ccei_ns <- rast(path(dir_ccei, "ccei.img"))
+ccei_245 <- rast(path(dir_ccei, "ccei_ssp245.tif"))
+ccei <- rast(path(dir_ccei, "ccei_ssp585.tif"))
+
+# Find really high values
+summary(ccei, size = 1000000)
+
+# Not very many, but huge!
+# In South America
+plot(ccei, type = "interval", breaks = c(0, 20, 100000))
+
+(box1 <- sf::st_bbox(c(xmin = -75, xmax = -50, ymin = -15, ymax = 0), crs = 4326)) %>%
+  ext() %>%
+  crop(x = ccei, y = .) %>%
+  plot(type = "interval", breaks = c(0, 1,3,5,10,15, 20, 100000))
+
+# Even closer
+(box2 <- sf::st_bbox(c(xmin = -72.4, xmax = -71, ymin = -2.8, ymax = -1.5), crs = 4326)) %>%
+  ext() %>%
+  crop(x = ccei, y = .) %>%
+  plot(type = "interval", breaks = c(0, 1, 3, 5, 10, 15, 20, 100000))
+
+sample <- crop(ccei, box2)
+
+# Grab future data and crop - 3.1 MB in inst/ext_data ... okay?
+
+f_future <- dir_ls(path(dir_future), glob = "*ssp585*")
+
+for(f in f_future) {
+  nm <- stringr::str_replace(path_file(f), "\\.tif", "_subsample.tif")
+  rast(f) %>%
+    crop(box2) %>%
+    writeRaster(path(dir_out_future, nm), overwrite = TRUE)
+}
+
+
+f_hist <- dir_ls(path(dir_hist), glob = "*.tif")
+for(f in f_hist) {
+  nm <- stringr::str_replace(path_file(f), "\\.tif", "_subsample.tif")
+  rast(f) %>%
+    crop(box2) %>%
+    writeRaster(path(dir_out_hist, nm), overwrite = TRUE)
+}
+
+# Add for comparison
+crop(ccei_245, box) %>%
+  writeRaster(path(out_dir, "ccei_ssp245.tif"), overwrite = TRUE)
+
+
+
+
