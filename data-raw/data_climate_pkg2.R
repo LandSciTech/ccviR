2f63dbba8742d06ec9929c5128adfc759a561102
Steffi LaZerte <steffi@steffi.ca>
2025-05-16 19:00:30 -0500

    Add README to clim processed files (part of #25)

--- data-raw/data_climate_pkg2.R
+++ data-raw/data_climate_pkg2.R
@@ -8,7 +8,7 @@ library(fs)
 scenarios <- c("ssp245", "ssp585")
 
 clim_raw <- path("misc", "climate", "raw")
-clim_prep <- path("misc", "climate", "prepared") %>%
+clim_prep <- path("misc", "climate", "processed") %>%
   dir_create()
 ccei <- dir_ls("misc/ccei") %>%
   fs::path_filter(regexp = paste0(paste0(scenarios, "\\.tif"), collapse = "|"))
@@ -19,7 +19,7 @@ ccei <- dir_ls("misc/ccei") %>%
 
 # Process Climate data -------------------------------------------------------
 
-prep_clim_data_multi(
+brks <- prep_clim_data_multi(
   mat_norm = path(clim_raw, "NB_norm_MAT.tif"),
   mat_fut = path(clim_raw, c("NB_RCP.4.5_MAT.tif", "NB_RCP.8.5_MAT.tif")),
   cmd_norm = path(clim_raw, "NB_norm_CMD.tif"),
@@ -33,6 +33,16 @@ prep_clim_data_multi(
   out_folder = clim_prep,
   overwrite = TRUE,
   scenario_name = c("RCP 4.5", "RCP 8.5"))
+
+prep_clim_readme(
+  scenario_name = c("RCP 4.5", "RCP 8.5"),
+  gcm_ensemble = "AdaptWest 15 CMIP5 AOGCM Ensemble",
+  hist_period = "1961-1990",
+  fut_period = "2050s",
+  emissions_scenario = c("RCP 4.5", "RCP 8.5"),
+  url = "https://adaptwest.databasin.org/pages/adaptwest-climatena-cmip5/",
+  out_folder = clim_prep,
+  brks = brks)
 
 # Checks...
 t0 <- terra::rast(ccei[1])
