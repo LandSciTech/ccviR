07f0445cc842223139dd97b3749d591f4c7aff73
Steffi LaZerte <steffi@steffi.ca>
2025-05-27 16:06:29 -0500

    Prep release instructions

---
+++ RELEASE.R
@@ -0,0 +1,65 @@
+# Prepare ccviR for release!!!
+
+
+# Update internal data sets --------------------
+source("data-raw/maps.R") # North American Context and example non-breeding
+source("data-raw/lookup_tbls.R")
+
+# Update/Create Large Supplemental data sets as needed ---------------
+# source("data-raw/ccei.R")
+# source("data-raw/data_climate_pkg2.R") / source("data-raw/data_climate_pkg.R") # Also re
+# source("data-raw/protected.R")
+
+# Update/Create Demo or testing datasets
+# source("data-raw/data_test.R")      # General test data
+# source("data-raw/data_ccei_mini.R") # CCEI data for tests and exploring large values
+# source("data-raw/data_demo.R")
+
+
+# Test coverage --------------------------------------------
+# Check for the entire suite, Actions only test non-shiny components
+covr::package_coverage(function_exclusions = c("^shiny", "^mod", "^run", "^ccvi_app"))
+
+
+# Documentation --------------------------------
+
+# - Update NEWS
+file.edit("NEWS.md")
+
+# - Check spelling
+dict <- hunspell::dictionary('en_CA')
+devtools::spell_check()
+spelling::update_wordlist()
+
+# - Update README.Rmd
+devtools::build_readme()
+
+# Finalize package version --------------------------------------
+
+# - Edit DESCRIPTION as needed
+file.edit("DESCRIPTION")
+
+# Final checks --------------------------------------------------
+
+# - Checks
+devtools::check(run_dont_test = TRUE)   # Local, run long-running examples
+
+# - Run in console - (Only if trying to be very persnickity)
+system("cd ..; R CMD build ccviR")
+system("cd ..; R CMD check ccviR_0.1.0.tar.gz --as-cran") # Update version
+
+# - Check GH Actions on GitHub
+
+
+# Release! ------------------------------------------------------
+
+# - Merge dev into master/main
+# - PULL updates to master/main
+# - Actually release it! Create signed release on GitHub
+usethis::use_github_release()
+
+# Get ready for next cycle --------------------------------------
+
+# - Create sandbox/dev branch
+# - Change API in data-raw/data_creation.R to sandbox
+# - Add dev components to version in DESCRIPTION and NEWS.md
