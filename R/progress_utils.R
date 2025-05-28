9310f8e1816fc6fff1f5ef3203150d5df14894c4
Steffi LaZerte <steffi@steffi.ca>
2025-05-28 11:06:32 -0500

    Avoid warnings if running parts of a shiny mod with messages

--- R/progress_utils.R
+++ R/progress_utils.R
@@ -1,7 +1,8 @@
-inform_prog <- function(detail, quiet, n = 100, set = FALSE) {
+inform_prog <- function(detail, quiet = FALSE, n = 100, set = FALSE) {
   if(quiet) return(invisible())
-
-  if(shiny::isRunning()) {
+  session <- getDefaultReactiveDomain()
+  # If running Shiny and running a section where Shiny progess messages have been set
+  if(shiny::isRunning() && session$progressStack$size() != 0) {
     # Sys.sleep(1) # Uncomment for testing progress messages interactively
     if(!set) incProgress(1/(n + 1), detail = detail)
     if(set) setProgress(n, detail = detail)
