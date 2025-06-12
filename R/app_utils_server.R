314452713f9a16dd4d641a7189a8cf186969b5c1
Steffi LaZerte <steffi@steffi.ca>
2025-06-12 13:31:02 -0500

    Catch false positive mismatch

--- R/app_utils_server.R
+++ R/app_utils_server.R
@@ -629,7 +629,8 @@ index_match_qs <- function(questions_wide, index) {
   qs <- questions_wide %>%
     dplyr::select(-dplyr::contains("com_"), -dplyr::contains("evi_")) %>%
     # Omit removed questions (not the same as unanswered)
-    dplyr::select(-dplyr::where(~all(.x == "") | all(.x == -1)))
+    dplyr::select(-dplyr::where(~all(.x == "") | all(.x == -1))) %>%
+    distinct()
 
   index <- select(index, matches("^[BCD]{1}\\d{1}[a-z]{0,3}")) %>%
     dplyr::select(-dplyr::where(~all(.x == "") | all(.x == -1))) %>%
